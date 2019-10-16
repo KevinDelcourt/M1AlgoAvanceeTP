31b5642d2e0dcaac56fc9bc3fa1d5dadfc3b6dd2
KevinDelcourt <delcokevgmail.com>
2019-10-16 17:02:40 +0200

    oui

--- TP1/bubbleSort.c
+++ TP1/bubbleSort.c
@@ -46,7 +46,7 @@ struct Activity monitoredLaunch(char *title, void (*doSort)(void))
 
 int main(int argc, char **argv)
 {
-    FILE *f_out;
+    FILE *f_out_time, *f_out_permut;
 
     read_data(argc, argv, &data, &n, &k);
     printf("Données : \n");
@@ -54,22 +54,30 @@ int main(int argc, char **argv)
 
     printf("\nRecherche des %d plus grands éléments du tableau de taille %d : \n", k, n);
 
-    /*f_out = fopen("out.csv", "w");
-    fprintf(f_out, "k,BubbleSort partiel,BuildHeap / Remove,HeapSort\n");
+    f_out_time = fopen("time.csv", "w");
+    f_out_permut = fopen("permut.csv", "w");
+
+    fprintf(f_out_time, "k,BubbleSort partiel,BuildHeap / Remove,HeapSort\n");
+    fprintf(f_out_permut, "k,BubbleSort partiel,BuildHeap / Remove,HeapSort\n");
     struct Activity activity;
     for (k = 1; k <= n; k++)
     {
         activity = monitoredLaunch("BubbleSort partiel", doBubbleSort);
-        fprintf(f_out, "%d,%.7f,", k, activity.timeSpent);
+        fprintf(f_out_time, "%d,%.7f,", k, activity.timeSpent);
+        fprintf(f_out_permut, "%d,%d,", k, activity.nbPermut);
         activity = monitoredLaunch("BuildHeap / remove", doBuildHeapRemove);
-        fprintf(f_out, "%.7f,", activity.timeSpent);
+        fprintf(f_out_time, "%.7f,", activity.timeSpent);
+        fprintf(f_out_permut, "%d,", activity.nbPermut);
         activity = monitoredLaunch("HeapSort", doHeapSort);
-        fprintf(f_out, "%.7f\n", activity.timeSpent);
-    }*/
-    monitoredLaunch("BubbleSort partiel", doBubbleSort);
+        fprintf(f_out_time, "%.7f\n", activity.timeSpent);
+        fprintf(f_out_permut, "%d\n", activity.nbPermut);
+    }
+    fclose(f_out_time);
+    fclose(f_out_permut);
+    /*monitoredLaunch("BubbleSort partiel", doBubbleSort);
     monitoredLaunch("BuildHeap / remove", doBuildHeapRemove);
     monitoredLaunch("HeapSort", doHeapSort);
-    monitoredLaunch("QuickSort", doQuickSort);
+    monitoredLaunch("QuickSort", doQuickSort);*/
 
     free(data);
     free(dataCopy);
